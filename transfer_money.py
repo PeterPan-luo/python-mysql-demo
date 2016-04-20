6cb39c0f6f9bb32fd51e92627f1bea7d946133d3
albert <luohong213@163.com>
2016-04-20 08:56:24 +0800

    python mysql demo

---
+++ transfer_money.py
@@ -0,0 +1,90 @@
+########################################################################################################
+# File Name: transfer_money.py
+# Author: luohong
+# mail:luohong213@163.com
+# Create Time: 2016年03月26日 星期六 18时37分48秒
+#==============================================================================================
+#!/usr/bin/python
+# coding:utf8
+import sys
+import MySQLdb
+
+class TransferMoney(object):
+    def __init__(self, conn):
+        self.conn = conn
+
+    def transfer(self, source_acctid, target_acctid, money):
+        try:
+            self.check_acct_available(source_acctid)
+            self.check_acct_available(target_acctid)
+            self.has_enough_money(source_acctid, money)
+            self.reduce_money(source_acctid, money)
+            self.add_money(target_acctid, money)
+            self.conn.commit()
+        except Exception as e:
+            self.conn.rollback()
+            raise e
+
+    def check_acct_available(self, acctid):
+        cursor = self.conn.cursor()
+        try:
+            sql = "select * from account where acctid=%s" %acctid
+            cursor.execute(sql)
+            print("check_acct_available:" + sql)
+            rs = cursor.fetchall()
+            if len(rs) != 1:
+                raise Exception("账户%s 不存在" % acctid)
+
+        finally:
+            cursor.close()
+    def has_enough_money(self, acctid, money):
+        cursor = self.conn.cursor()
+        try:
+            sql = "select * from account where acctid=%s and money > %s" %(acctid, money)
+            cursor.execute(sql)
+            print("has_enough_money:" + sql)
+            rs = cursor.fetchall()
+            if len(rs) != 1:
+                raise Exception("账户%s 没有足够的钱" % acctid)
+
+        finally:
+            cursor.close()
+
+    def reduce_money(self, acctid, money):
+        cursor = self.conn.cursor()
+        try:
+            sql = "update account set money = money - %s where acctid=%s" %(money,acctid)
+            cursor.execute(sql)
+            print("reduce_money:" + sql)
+            if len(cursor.rowcount) != 1:
+                raise Exception("账户%s 减款失败" % acctid)
+
+        finally:
+            cursor.close()
+    def add_money(self, acctid, money):
+        cursor = self.conn.cursor()
+        try:
+            sql = "update account set money = money + %s where acctid=%s" %(money,acctid)
+            cursor.execute(sql)
+            print("add_money:" + sql)
+            if len(cursor.rowcount) != 1:
+                raise Exception("账户%s 加款失败" % acctid)
+
+        finally:
+            cursor.close()
+
+
+if __name__ == "__main__":
+    source_acctid = sys.argv[1]
+    target_acctid = sys.argv[2]
+    money = sys.argv[3]
+
+    conn = MySQLdb.Connect(host='192.******', user='root', passwd='*****')
+    tr_money = TransferMoney(conn)
+
+    try:
+        tr_money.transfer(source_acctid, target_acctid, money)
+    except Exception as e:
+        print("出现的问题：" + str(e))
+    finally:
+        conn.close()
